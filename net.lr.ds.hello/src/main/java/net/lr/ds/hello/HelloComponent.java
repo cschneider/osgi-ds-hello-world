a065d050a7aa82a143dbb1bd3d12688ac97042ae
Christian Schneider <cschneid@adobe.com>
2018-08-08 10:10:24 +0200

    Add logging to the component

--- net.lr.ds.hello/src/main/java/net/lr/ds/hello/HelloComponent.java
+++ net.lr.ds.hello/src/main/java/net/lr/ds/hello/HelloComponent.java
@@ -2,12 +2,15 @@ package net.lr.ds.hello;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class HelloComponent {
+	Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Activate
 	public void start() {
-		System.out.println("Hello World");
+		log.info("Hello World");
 	}
 }
